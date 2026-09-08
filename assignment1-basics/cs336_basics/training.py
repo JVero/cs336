3401c4cb7128c3e230fd0aded2ee874fb728c882
Joseph Vero <joev714@gmail.com>
2026-09-08 17:57:11 -0400

    A1: AdamW

--- assignment1-basics/cs336_basics/training.py
+++ assignment1-basics/cs336_basics/training.py
@@ -36,6 +36,39 @@ class SGD(torch.optim.Optimizer):
                 state["t"] = t + 1 # Increment iteration number.
         return loss
 
+ 
+
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(self, params, weight_decay=0.01, lr=1e-3, betas=(0.9, 0.999), eps=1e-8):
+        # LR is the same as alpha
+        if lr < 0:
+            raise ValueError(f"Invalid learning rate {lr}")
+        defaults = {"gamma": weight_decay,
+                    "lr": lr, 
+                    "b1": betas[0], 
+                    "b2": betas[1], 
+                    "eps": eps}
+        super().__init__(params, defaults)
+            
+    def step(self, closure: Optional[Callable] = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            gamma, lr, b1, b2, eps = (group.get(k) for k in ["gamma", "lr", "b1", "b2", "eps"])
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+                t = state.get("t", 1)
+                g = p.grad.data # Line 6
+                at = lr * math.sqrt(1-b2 ** t) / (1 - b1**t) # Line 7
+                p.data -= lr * gamma * p.data # Line 8 - Weight Decay
+                state["m"] = b1 * state.get("m", torch.zeros_like(p.data)) + (1 - b1) * g # Lines 2 and 9
+                state["v"] = b2 * state.get("v", torch.zeros_like(p.data)) + (1 - b2) * g*g # Lines 3 and 10
+                p.data -= at * state["m"]/(eps + torch.sqrt(state["v"])) # Line 11
+                state["t"] = t+1 # Incrementing, has to do with line 4
+        return loss
+    
 def test_lrs():
     lrs = [1e1, 1e2, 1e3]
     for lr in lrs:
@@ -48,6 +81,3 @@ def test_lrs():
             print(round(loss.cpu().item(), 2))
             loss.backward()
             opt.step()    
-
-if __name__ == "__main__":
-    test_lrs()
